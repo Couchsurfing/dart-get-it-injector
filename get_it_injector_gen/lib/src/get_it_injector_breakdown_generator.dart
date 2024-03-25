168268ddb12f5a0eb321474ee9f40df149d37acf
Morgan <mrgnhnt96@gmail.com>
2024-03-25 11:59:19 -0700

    feat: update beginning and end of license identifiers

--- get_it_injector_gen/lib/src/get_it_injector_breakdown_generator.dart
+++ get_it_injector_gen/lib/src/get_it_injector_breakdown_generator.dart
@@ -1,5 +1,5 @@
+// --- LICENSE ---
 /**
---- LICENSE ---
 Copyright 2024 CouchSurfing International Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,8 +13,8 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
---- LICENSE ---
 */
+// --- LICENSE ---
 import 'dart:async' show FutureOr;
 
 import 'package:build/build.dart';
--- get_it_injector_test/test/main_test.dart
+++ get_it_injector_test/test/main_test.dart
@@ -1,5 +1,5 @@
+// --- LICENSE ---
 /**
---- LICENSE ---
 Copyright 2024 CouchSurfing International Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,8 +13,8 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
---- LICENSE ---
 */
+// --- LICENSE ---
 import 'package:_application/blocs/user_bloc.dart';
 import 'package:_application/setup.dart' as application;
 import 'package:_domain/domain.dart';
