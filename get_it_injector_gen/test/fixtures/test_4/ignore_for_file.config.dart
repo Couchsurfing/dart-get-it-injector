ff4885401273bf8f88c4bbdf422c51767db102ba
Morgan <mrgnhnt96@gmail.com>
2024-03-25 12:52:50 -0700

    fix: broken tests

--- get_it_injector_gen/test/fixtures/test_4/ignore_for_file.config.dart
+++ get_it_injector_gen/test/fixtures/test_4/ignore_for_file.config.dart
@@ -4,7 +4,8 @@
 // GetItInjectorGenerator
 // **************************************************************************
 
-// ignore_for_file: lint_1, lint_2
+// ignore: lines_longer_than_80_chars
+// ignore_for_file: lint_1,lint_2
 import 'package:get_it/get_it.dart';
 import 'package:a/test/inputs/test_1/repo.dart' as i0;
 
