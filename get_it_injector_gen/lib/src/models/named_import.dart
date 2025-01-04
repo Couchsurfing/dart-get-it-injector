2f8330b11b7fc4208fea14fabb9d4ddcbc020511
Morgan <mrgnhnt96@gmail.com>
2025-01-03 19:16:35 -0700

    feat: update year

--- get_it_injector_gen/lib/src/models/named_import.dart
+++ get_it_injector_gen/lib/src/models/named_import.dart
@@ -1,6 +1,6 @@
 // --- LICENSE ---
 /**
-Copyright 2024 CouchSurfing International Inc.
+Copyright 2025 CouchSurfing International Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
